3b44a7e3ceb7040b6f58530ae1cc64a146755bcb
Juliana Bearzi <julietad299@gmail.com>
2021-12-15 23:00:25 -0300

    style changes in shared components

--- src/components/Shared/TextInput/index.jsx
+++ src/components/Shared/TextInput/index.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import TextField from '@mui/material/TextField';
 import styles from './textInput.module.css';
 
-const TextInput = ({ input, label, placeholder, meta }) => {
+const TextInput = ({ input, label, placeholder, meta, size, variant }) => {
   return (
     <div className={styles.inputContainer}>
       <label className={styles.lbl}>{label}</label>
-      <input {...input} placeholder={placeholder} />
+      <TextField
+        {...input}
+        size={size}
+        variante={variant}
+        placeholder={placeholder}
+      />
       {meta.error && meta.touched && (
         <div className={styles.error}>{meta.error}</div>
       )}
