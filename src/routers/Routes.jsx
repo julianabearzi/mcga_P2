55d5c160c590607018bf59f8674b258c7452d1d2
Juliana Bearzi <julietad299@gmail.com>
2021-12-17 10:16:45 -0300

    routing changes &  add public route and private route

--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Layout from '../components/Layout';
+import { connect } from 'react-redux';
 import Home from '../components/Home';
+import Layout from '../components/Layout';
+import Login from '../components/Login';
+import Users from '../components/Users';
 import Students from '../components/Students';
+import PrivateRoute from './PrivateRoute';
+import RoutePublic from './PublicRoute';
 
-const Routes = () => {
+const Routes = ({ username }) => {
   return (
     <Switch>
       <Redirect exact push from="/" to="/home" />
@@ -13,18 +18,26 @@ const Routes = () => {
           <Home />
         </Layout>
       </Route>
-      <Route exact path="/students">
-        <Layout>
-          <Students />
-        </Layout>
-      </Route>
-      <Route exact path="*">
-        <Layout>
-          <h2>Not Found</h2>
-        </Layout>
-      </Route>
+      <RoutePublic
+        exact
+        path="/login"
+        component={Login}
+        authenticated={!!username}
+      />
+      <RoutePublic
+        exact
+        path="/signup"
+        component={Users}
+        authenticated={!!username}
+      />
+      <PrivateRoute exact path="/students" component={Students} />
+      <Redirect exact push from="*" to="/home" />
     </Switch>
   );
 };
 
-export default Routes;
+const mapStateToProps = (state) => ({
+  username: state.auth.username,
+});
+
+export default connect(mapStateToProps, null)(Routes);
